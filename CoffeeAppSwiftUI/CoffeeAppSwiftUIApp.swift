892a8eb5bd75f92b2dc47f18b05171e391c354c9
Brian Surface <brian.surface@outlook.com>
2024-08-03 08:01:43 -0400

    Initial Commit

---
+++ CoffeeAppSwiftUI/CoffeeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppSwiftUIApp.swift
+//  CoffeeAppSwiftUI
+//
+//  Created by Brian Surface on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
